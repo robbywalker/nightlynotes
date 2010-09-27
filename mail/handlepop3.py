6560d5f5c5cac235240f84a96e845fcab9406958
Christine Yen <cyen@christineyen.com>
2010-09-26 17:43:20 -0700

    Escape body text correctly for database updates.

--- mail/handlepop3.py
+++ mail/handlepop3.py
@@ -62,9 +62,8 @@ class POP3DownloadFactory(protocol.ClientFactory):
   def handleMessage(self, messageData):
     parsedMessage = email.message_from_string(messageData)
     token = re.search(r'\[token (.*)\]', parsedMessage.get('Subject'))
-    log.msg("parsed token %s from subject: %s" % (
-      str(token.group(1)), parsedMessage.get('Subject')))
     if token is None:
+      log.msg('Missing token from subject: %s' % (parsedMessage.get('Subject')))
       return
     body = parsedMessage.get_payload()[0].get_payload().strip()
     self._getUserAndDateFromToken(token.group(1), body)
@@ -73,8 +72,8 @@ class POP3DownloadFactory(protocol.ClientFactory):
     log.msg(reason)
 
   def _getUserAndDateFromToken(self, token, body):
-    query = "SELECT user_id, date FROM token WHERE token = '%s'" % (token)
-    queryDefer = self.__dbpool.runQuery(query)
+    query = 'SELECT user_id, date FROM token WHERE token = ?'
+    queryDefer = self.__dbpool.runQuery(query, (token,))
     queryDefer.addCallback(
       self._createEntry, body).addErrback(
       self._failed, 'Token not found: ' + token)
@@ -88,19 +87,16 @@ class POP3DownloadFactory(protocol.ClientFactory):
     date = result[0][1]
     log.msg('Creating entry for user %s and date %s' % (user_id, date))
     query = '''
-      INSERT OR ABORT INTO user_entry (user_id, date, text)
-      VALUES (%s, '%s', '%s')
-    ''' % (user_id, date, body)
-    queryDefer = self.__dbpool.runQuery(query)
+      INSERT OR ABORT INTO user_entry (user_id, date, text) VALUES (?, ?, ?)
+    '''
+    queryDefer = self.__dbpool.runQuery(query, (user_id, date, body))
     queryDefer.addErrback(self._insertUserEntryFailed, user_id, date, body)
     return queryDefer
 
   def _insertUserEntryFailed(self, failure, user_id, date, body):
     log.msg('Fetching existing user_entry for %s on %s' % (user_id, date))
-    query = '''
-      SELECT text FROM user_entry WHERE user_id = %s AND date = '%s'
-    ''' % (user_id, date)
-    queryDefer = self.__dbpool.runQuery(query)
+    query = 'SELECT text FROM user_entry WHERE user_id = ? AND date = ?'
+    queryDefer = self.__dbpool.runQuery(query, (user_id, date))
     queryDefer.addCallback(
       self._appendEntry, user_id, date, body).addErrback(
       self._failed, 'Query: ' + query)
@@ -113,10 +109,9 @@ class POP3DownloadFactory(protocol.ClientFactory):
       return
 
     log.msg('Updating entry for user %s and date %s' % (user_id, date))
-    query = '''
-      UPDATE user_entry SET text = '%s\n%s' WHERE user_id = %s AND date = '%s'
-    ''' % (result[0][0], body, user_id, date)
-    queryDefer = self.__dbpool.runQuery(query)
+    query = 'UPDATE user_entry SET text = ? WHERE user_id = ? AND date = ?'
+    newBody = '%s\r\n\r\n%s' % (result[0][0], body)
+    queryDefer = self.__dbpool.runQuery(query, (newBody, user_id, date))
     queryDefer.addErrback(self._failed, 'Query: ' + query)
     return queryDefer
 
